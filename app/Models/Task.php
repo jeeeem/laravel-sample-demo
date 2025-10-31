e8b09c2aeb6ff27596a176798139a37c391c0fbe
Jim Angelo Beren <jim.beren07@gmail.com>
2025-11-01 07:29:25 +0800

    refactor: remove deprecated task status constants from Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -17,17 +17,6 @@ class Task extends Model
     use HasFactory;
 
     /**
-     * Task status constants (maintained for backward compatibility)
-     *
-     * @deprecated Use TaskStatus enum instead
-     */
-    public const STATUS_PENDING = 'pending';
-
-    public const STATUS_IN_PROGRESS = 'in_progress';
-
-    public const STATUS_COMPLETED = 'completed';
-
-    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
